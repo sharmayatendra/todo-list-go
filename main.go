6da8e8bd9d271463f484970a198e0b4b3d98d5c0
sharmayatendra <yatendra948@gmail.com>
2024-12-16 04:04:50 +0530

    loops

--- main.go
+++ main.go
@@ -9,12 +9,19 @@ func main() {
 	// alternative declaration syntax for variables
 	secondVariable := "second variable"
 
+	thirdVariable := "third variable"
+
 	// slices syntax:
-	combinedVariables := []string {firstVariable, secondVariable }
+	combinedVariables := []string {firstVariable, secondVariable, thirdVariable }
 	
 	// so diff b/w array & slice is array is fixed size while slice is dynamically allocated
 	// example:
 	// listArray := [2]string {firstVariable, secondVariable, secondVariable}
 	// above code will throw an error as it is strict for 2 elements
-	fmt.Println(combinedVariables)
+	// fmt.Println(combinedVariables)
+
+	// loops syntax:
+	for index, task := range combinedVariables {
+		fmt.Printf("%d. %s\n", index + 1, task)
+	}
 }
