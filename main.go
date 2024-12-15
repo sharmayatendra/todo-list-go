17e08b8b73a8dd88bdb35e119024529ad9d0387a
sharmayatendra <yatendra948@gmail.com>
2024-12-16 03:17:51 +0530

    var decleartion second type

--- main.go
+++ main.go
@@ -5,5 +5,9 @@ import "fmt"
 func main() {
 	// using variables
 	var firstVariable = "first variable"
+
+	// alternative declaration syntax for variables
+	secondVariable := "second variable"
 	fmt.Println(firstVariable);
+	fmt.Println((secondVariable))
 }
